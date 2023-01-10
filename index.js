39a0d0421795b614e2efd43a870631578a24d3b3
EmilioC1 <93024518+EmilioC1@users.noreply.github.com>
2023-01-10 14:33:20 -0500

    chanegs

--- index.js
+++ index.js
@@ -19,18 +19,50 @@ app.use(bodyParser.json())
 
 const getTweets = async () => {
   // todo: get all tweets
+  try {
+      await mongoose;
+      const result = await Tweet.find({}).exec();
+      // console.log(`here are all of the tweets: ${result}`);
+      return result;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 const getUserTweets = async username => {
   // todo: get all tweets from a specific user
+  try {
+    // await mongoose;
+    // console.log('just obtained all of the tweets');
+    return await Tweet.find({username: username}).exec();
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 const createTweet = async (username, text) => {
   // todo: create a tweet with a username and text
+  try {
+    // await mongoose;
+    const tweet = new Tweet({username: username, text: text});
+    tweet.save();
+    // console.log('just created a tweet');
+    return tweet;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 const deleteTweet = async id => {
   // todo: delete a single tweet, given the id
+  try {
+    // await mongoose;
+    var result = await Tweet.findByIdAndDelete(id).exec();
+    console.log(`just deleted the tweets with the id ${id}`);
+    return result;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 /*
@@ -38,7 +70,7 @@ const deleteTweet = async id => {
  */
 app.get('/api/tweet', async (req, res) => {
   const username = req.query.username
-
+  console.log('did we get to the api/tweet GET command?');
   let tweets
   if (username === undefined)
     tweets = await getTweets()
@@ -50,7 +82,7 @@ app.get('/api/tweet', async (req, res) => {
 app.post('/api/tweet', async (req, res) => {
   const text = req.body.text
   const username = req.body.username
-
+  console.log('did we get to the api/tweet POST command?');
   const tweet = await createTweet(username, text)
   res.json(tweet)
 })
